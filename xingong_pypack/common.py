9adfaa48d3de65c3e36e84716c9ace9074e49399
xingongli <lixi@ku.edu>
2024-08-16 09:17:20 -0500

    Add a new function hello()

--- xingong_pypack/common.py
+++ xingong_pypack/common.py
@@ -5,3 +5,11 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+def hello(name):
+    """Prints "Hello, <name>!" to the console.
+
+    Args:
+        name (str): The name to say hello to.
+    """
+    print(f"Hello, {name}!")
